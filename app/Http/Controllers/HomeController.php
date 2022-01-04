8adf64f37a207a9771c5ef4510489daed13c7699
Yaroslav Ahulov <yaroslavagulov2002@gmail.com>
2022-01-04 08:54:39 +0200

    configuring routes after login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Form;
+use Illuminate\Support\Facades\Auth;
+
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+
+        if(Auth::user()->role == 'user') {
+            return view('user');
+        }
+        elseif (Auth::user()->role == 'admin'){
+            return view('admin');
+        }
+        else return abort(404);
     }
+
 }
