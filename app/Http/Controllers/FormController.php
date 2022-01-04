5038066836b115203b168434c6dd99bb5e4ffcdb
Yaroslav Ahulov <yaroslavagulov2002@gmail.com>
2022-01-04 12:09:44 +0200

    make form validation

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -5,21 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Form;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Validator;
 
 
 class FormController extends Controller
 {
-   /* public function validator(array $data)
-    {
-        return Validator::make($data, [
+    //Да этот код можно было бы сократить, но видимо делал это неправильно, так как у меня выдавало странные ошибки
+    public function store(Request $request){
+
+        $validator = Validator::make($request->only('title', 'body', 'file'), [
             'title' => ['required', 'string', 'min:5', 'max:50'],
             'body' => ['required', 'string', 'max:255'],
             'file'  => ['image',' mimes:jpeg,jpg,png'],
-        ]);
-    }*/
-    //Да этот код можно было бы сократить, но видимо делал это неправильно, так как у меня выдавало странные ошибки
-    public function store(Request $request){
+        ])->validate();
+
+
         $data = $request->only('title', 'body', 'file', 'user_id');
         if($request->file('file')){
             $file = $request->file('file')->store('uploads', 'public');
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,14 +10,23 @@
                 <label for="title">Тема</label>
                 <input type="text" class="form-control" name="title" id="title">
             </div>
+            @error('title')
+            <div class="alert alert-danger mt-2">{{ $message }}</div>
+            @enderror
             <div class="form-group">
                 <label for="body">Сообщение</label>
                 <textarea class="form-control" name="body" id="body" rows="4"></textarea>
             </div>
+            @error('body')
+            <div class="alert alert-danger mt-2">{{ $message }}</div>
+            @enderror
             <div class="form-group mt-3">
                 <label for="file">Файловый импут</label>
                 <input type="file" class="form-control-file" name="file" id="file">
             </div>
+            @error('file')
+            <div class="alert alert-danger mt-2">{{ $message }}</div>
+            @enderror
 
             <button type="submit" class="btn btn-primary mt-3">Подтвердить</button>
         </form>
